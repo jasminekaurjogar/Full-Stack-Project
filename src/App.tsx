6e85024717e69f2f6ba5a1840a90816b3ffcdf79
sehajpreetkaurbhullar <bitwarden2000@gmail.com>
2026-09-07 20:36:39 -0500

    Add starter MoneyMap app component

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>MoneyMap</h1>
+        <p>A simple personal finance tracker for Sprint 1.</p>
+      </header>
+
+      <main>
+        <h2>Welcome to MoneyMap</h2>
+        <p>
+          This app will help users organize expenses, savings goals, and money
+          habits.
+        </p>
+      </main>
+
+      <footer>
+        <p>Created by Sehajpreet Kaur and Jasmine Kaur</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
